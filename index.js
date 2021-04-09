fc3d7f89fb75abd137c0b09d911d5c0597284e0f
Daniel Barahona <>
2021-04-09 18:52:22 +0000

    Completed

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ var customers = require('./customers.json');
 
 
 
+
 /*
 1. Return the all friends of a given customers name.
 When returning the friends of the customer it
@@ -13,11 +14,27 @@ should be a string and NOT the friends array.
 
 function friendsOfCustomer(name){
   //CODE BELOW HERE
+ 
+// 1. We are dealing with an array datatype: Is there a way to
+// look at each individual elements? What can you use?
+
+//What condition needs to be true for a friends list to print?
+
+// What should happen if you type your name into the function?
+
+// should a friends list print? yes or no
 
 
-
-  //CODE ABOVE HERE
+for( i = 0; i <= customers.length - 1; i++){
+ 
+if(name === customers[i].name){
+  
+  }
 }
+
+ //CODE ABOVE HERE
+};
+console.log(friendsOfCustomer('Shelly Walton'))
 
 
 
@@ -25,14 +42,24 @@ function friendsOfCustomer(name){
 
 /*
 2. Return the phone number and address of a 
-given customer when thier name is entered into the 
+given customer when their name is entered into the 
 function.
 */
 
+//Input:
+//Output:
+//Constraints:
+//Edge Case:
+
 function contactInfo(name){
   //CODE BELOW HERE
+for(i = 0; i <= customers.length - 1; i++){
+  if(name === customers[i].name){
 
+  }
+}
 
 
   //CODE ABOVE HERE
-}
+};
+console.log(contactInfo('+1 (806) 578 2712'))
